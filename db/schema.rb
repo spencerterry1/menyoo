4df60af6139aab3a88add91f623f62e91c981843
Walter Cegarra <walter.cegarra@icloud.com>
2019-08-31 11:45:02 +0100

    new branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2019_08_27_193655) do
-
+ActiveRecord::Schema.define(version: 2019_08_29_192502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
