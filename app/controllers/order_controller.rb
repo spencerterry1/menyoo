069d119fc88a226398f6cd2941ab4bc45d009783
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-03 20:32:14 +0100

    added default values to Attendee and Order models

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,20 +1,31 @@
 class OrderController < ApplicationController
- def show
- end
 
- def new
- end
+  before_action :set_attendee, except: [:destroy]
 
- def create
- end
+  def show
+  end
 
- def edit
- end
+  def new
+  end
 
- def update
- end
+  def create
+  end
 
- def destroy
- end
+  def edit
+  end
 
+  def update
+  end
+
+  def destroy
+  end
+
+end
+
+def order_params
+  params.require(:order).permit(:dish_id, :price, :quantity, :ordered)
+end
+
+def set_attendee
+  @attendee = current_user
 end
--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,4 +2,12 @@ class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :booking
   has_many :orders
+
+  before_save :default_values
+
+  def default_values
+    self.accepted = false if self.accepted.nil?
+    self.payment = false if self.payment.nil?
+  end
+
 end
