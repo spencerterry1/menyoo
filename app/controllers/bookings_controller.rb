62e81f8a0283c8f4125344859bdec204cc3c574c
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-07 10:39:54 +0100

    Updated @attendee_user to make reference to specifc Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,7 +6,8 @@ class BookingsController < ApplicationController
     @users = User.all
     @attendee = Attendee.new
 
-    @attendee_user = current_user.attendees.last
+    # @attendee_user = current_user.attendees.last
+    @attendee_user = Attendee.where(user: current_user, booking: @booking)
 
     # calls search_for_users method if a user tries to type in the search bar to add another use to the table
     search_for_users if params[:search]
