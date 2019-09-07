b3d178c37eb7564bb1ca7ee028735e8b7bfa7c3e
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-07 14:04:17 +0100

    Moved methods to Applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  # Returns an array of Open Bookings for the given User
+  def bookings_open_for_user(user)
+    bookings_open = []
+    # All Attendees for User where User has accepted:
+    attendees_user_accepted = Attendee.where(user: user, accepted: true)
+    # Create array of Bookings if Booking for that restaurant and Booking open
+    attendees_user_accepted.each do |attendee|
+      if attendee.booking.open == true
+        bookings_open << attendee.booking
+      end
+    end
+    return bookings_open
+  end
+end
 
+# Returns an array of Bookings: all the open Bookings for that User, where the User has accepted to be an Attendee.
+def booking_open_for_user_restaurant(user, restaurant)
+  # Using method defined in Application controller
+  bookings_open_restaurant = []
+  bookings_open_user = bookings_open_for_user(user)
+  # Create array of Bookings if Booking for that restaurant and Booking open
+  bookings_open_user.each do |booking|
+    if booking.restaurant == restaurant
+      bookings_open_restaurant << booking
+    end
+  end
+  return bookings_open_restaurant
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,25 +22,11 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     if user_signed_in?
       @attendee = current_user.attendees.last
-      @booking_array = booking_open(current_user, @restaurant)
+      @booking_array = booking_open_for_user_restaurant(current_user, @restaurant)
       @booking = @booking_array.last
       @order = Order.new
     end
   end
 
-  private
 
-  # Returns an array of Bookings: all the open Bookings for that User, where the User has accepted to be an Attendee.
-  def booking_open(user, restaurant)
-    bookings_open = []
-    # All Attendees for User where User has accepted:
-    attendees_user_accepted = Attendee.where(user: user, accepted: true)
-    # Create array of Bookings if Booking for that restaurant and Booking open
-    attendees_user_accepted.each do |attendee|
-      if attendee.booking.restaurant == restaurant && attendee.booking.open == true
-        bookings_open << attendee.booking
-      end
-    end
-    return bookings_open
-  end
 end
