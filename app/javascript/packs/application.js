91e0e6a06f89c58779402549fbf7946f1892bd73
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-10 21:18:07 +0100

    added javascript to toggle between map and featured restaurants on home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,14 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+// Toggle showing Restaurants on Map vs Featured on Home page
+const mapButton = document.querySelector(".map-button");
+const featuredSection = document.querySelector(".home-featured-section");
+const mapSection = document.querySelector(".home-map-section");
+mapButton.addEventListener("click", (event) => {
+  mapSection.classList.toggle("show");
+  mapSection.classList.toggle("hide");
+  featuredSection.classList.toggle("show");
+  featuredSection.classList.toggle("hide");
+});
