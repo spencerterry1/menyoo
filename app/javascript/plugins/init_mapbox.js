0ccf1954076c59185207b252be19a478006839ed
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-10 20:46:26 +0100

    Home page - added map and markers

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,17 @@
+// app/javascript/plugins/init_mapbox.js
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+  }
+};
+
+export { initMapbox };
