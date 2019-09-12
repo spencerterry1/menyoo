f6fffd8fb3cbc8a06742084e3803fc1e4e9e488d
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-12 18:30:25 +0100

    issue with info windows

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,6 +15,7 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
