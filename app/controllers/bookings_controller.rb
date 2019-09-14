cc0a2fb0e80e0dfcba7087931f93d2693f19c392
Walter Cegarra <walter.cegarra@icloud.com>
2019-09-14 14:51:17 +0100

    implemented logic for checkin button updating booking status

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -54,6 +54,14 @@ class BookingsController < ApplicationController
     end
   end
 
+  def checkin
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @booking = Booking.find(params[:booking_id])
+    @booking.checkedin = true
+    @booking.save
+    redirect_to restaurant_bookings_path(@restaurant)
+  end
+
   def update
     @booking = Booking.find(params[:id])
     @restaurant = Restaurant.find(params[:restaurant_id])
