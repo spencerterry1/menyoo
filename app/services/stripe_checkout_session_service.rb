1f4767e0990e870cc837d7c0c6e994dadd40a217
Spencer Terry <spencerterry1@gmail.com>
2019-10-13 16:24:47 +0100

    added ability to close bookings

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     payment = Payment.find_by(checkout_session_id: event.data.object.id)
+
     attendees_not_paid = payment.booking.attendees.where(payment: false)
     attendees_not_paid_sum = 0.0
     attendees_not_paid.each do |attendee|
@@ -12,8 +13,12 @@ class StripeCheckoutSessionService
     if (event.data.object.display_items[0].amount / 100.0).round(2) == attendees_not_paid_sum
       payment.booking.attendees.update_all(payment: true)
       payment.booking.open = false
+
     else
       payment.attendee.update(payment: true)
+      if attendees_not_paid == false
+        payment.booking.open = false
+      end
     end
   end
 end
