1ad4af6852318c2186d44248728cdb02094a7553
Spencer Terry <spencerterry1@gmail.com>
2020-05-31 21:38:17 +0100

    remove comments page from dashboard

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -130,7 +130,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -323,7 +323,7 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
+  config.footer = 'Powered by Menyoo'
 
   # == Sorting
   #
